aa737a3d812d26c62d4470a414a84b0e5d521fc0
maher <gomemahero.com>
2022-05-23 21:43:16 +0300

    infinite scroll practice

--- src/pages/home.js
+++ src/pages/home.js
@@ -3,6 +3,8 @@ import axios from 'axios';
 import { useState,useEffect } from 'react';
 import { fetch_userinfo } from '../redux/user';
 import { useDispatch,useSelector } from 'react-redux';
+import InfiniteScroll from "react-infinite-scroll-component";
+import AllLectures from '../components/lecture/allLectures';
 
 const Home = () => {
 
@@ -13,6 +15,38 @@ const Home = () => {
     const {userinfo} = useSelector(state => state.user);
 
 const { allcourses } = useSelector(state => state.course);
+
+
+let pageSize = 5
+const [entries, setEntries] = useState([])
+const [offset, setOffset] = useState(0)
+const [hasMore, setHasMore] = useState(true)
+const [scroll, setScroll] = useState(false)
+
+// InfiniteScroll only for visualization since all files are loaded async at once.
+const fetchData = async () => {
+  try {      
+    const newEntries = allcourses.slice(0, offset + 5);      
+    setEntries(newEntries)
+    setOffset((prevOffset) => prevOffset + pageSize)      
+    entries.length === allcourses.length && setHasMore(false)             
+  } catch (error) {
+    console.log(error)
+  }
+}
+
+
+useEffect(() => {
+
+  
+       
+        
+
+        fetchData()
+      
+    
+  }, [])
+
 
 
 
@@ -30,6 +64,25 @@ const { allcourses } = useSelector(state => state.course);
      
 {/* -contetnt----- */}
 <div>
+
+<InfiniteScroll
+          dataLength={allcourses.length}
+          next={fetchData}
+          hasMore={hasMore}
+          loader={<h4>Loading...</h4>}
+        >
+{entries.map((course) => (
+    
+<div className=' mb-[120px]'>
+  <img src={course.image}
+alt="" />
+</div>
+
+))}
+
+
+</InfiniteScroll>
+
 
 
 <div>
